dc28c70f77793f8f9f1750cee8d16e5fdeb193c8
BIKRAM\bikram <bikrambehera@gmail.com>
2024-11-11 19:50:01 +0530

    Fixed env var issue REACT_APP_

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,10 +1,9 @@
 const conf = {
-  appwriteUrl: String(import.meta.env.APPWRITE_URL),
-  appwriteProjectId: String(import.meta.env.APPWRITE_PROJECT_ID),
-  appwriteDatabaseId: String(import.meta.env.APPWRITE_DATABASE_ID),
-  appwriteCollectionId: String(import.meta.env.APPWRITE_COLLECTION_ID),
-  appwriteBucketId: String(import.meta.env.APPWRITE_BUCKET_ID),
+  appwriteUrl: String(process.env.APPWRITE_URL),
+  appwriteProjectId: String(process.env.APPWRITE_PROJECT_ID),
+  appwriteDatabaseId: String(process.env.APPWRITE_DATABASE_ID),
+  appwriteCollectionId: String(process.env.APPWRITE_COLLECTION_ID),
+  appwriteBucketId: String(process.env.APPWRITE_BUCKET_ID),
 }
-// there was a name issue with the import.meta.env.VITE_APPWRITE_URL, it was later fixed in debugging video
 
 export default conf
