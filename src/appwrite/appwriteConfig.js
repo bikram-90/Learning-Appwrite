031585ba4b2c53db26a95dda7d8b00e5fe0a606a
Bikram Kumar Behera <bikrambehera7@gmail.com>
2024-11-11 18:51:55 +0530

    Update appwriteConfig.js

--- src/appwrite/appwriteConfig.js
+++ src/appwrite/appwriteConfig.js
@@ -2,10 +2,10 @@ import { Client, Account, Databases } from 'appwrite';
 
 const client = new Client();
 
-client.setEndpoint("http://localhost/v1").setProject("6730be9b0023c1304434");
+client.setEndpoint("https://cloud.appwrite.io/v1").setProject("6732049200211607b168");
 
 export const account = new Account(client);
 
 //Database
 
-export const databases = new Databases(client, "6730c022000fe0dcca18");
+export const databases = new Databases(client, "673204df000a5683bcf9");
